de14e0164d7366e70eed8fab9228ea1bd5b06eca
MahriNizar <mahrinizar01@gmail.com>
2020-09-25 12:40:13 +0200

    t

---
+++ guess_game.py
@@ -0,0 +1,30 @@
+import random
+random.seed(1)
+to_guess = random.randint(0,100)
+essaie = 6
+print(to_guess)
+guess = int(input())
+
+compt = 1
+if guess == to_guess:
+    print(f"Gagné en 1 essais !")
+
+elif guess != to_guess:
+    for i in range(5):
+        compt = compt + 1
+         
+        if guess < to_guess:
+            print("Trop petit")
+            guess = int(input())
+        else:
+            print("Trop grand")
+            guess = int(input())
+        if guess == to_guess:
+            print(f"Gagné en {compt} essais !")
+            break
+
+if guess != to_guess:
+    print(f"Perdu ! Le secret était {to_guess}")
+
+    
+
