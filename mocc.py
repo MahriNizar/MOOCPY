029ffb80e19e0b98e93f754d5ba1707d07f812af
mahrinizar <mahrinizar01@gmail.com>
2020-10-01 11:44:53 +0200

    changes

--- mocc.py
+++ mocc.py
@@ -5,10 +5,10 @@ def rendre_monnaie(prix,x20,x10,x5,x2,x1):
         return (None,None,None,None,None)
     else:
         somme_argent -= prix
-        b20 = (somme_argent -(somme_argent % 20))/20
-        b10 = int(10-((somme_argent%20) %10))
-        b5 = int((somme_argent%20)/5)
-        b2 = int((somme_argent%20)/2)
+        b20 = int((somme_argent -(somme_argent % 20))/20)  #16
+        b10 = int((somme_argent%20)/10)#6
+        b5 = int(((somme_argent%20)%10)/5)
+        b2 = int((((somme_argent%20)%10)%5)/2)
         b1 = ((((somme_argent%20) %10)%5)%2)
         return (b20,b10,b5,b2,b1)
 print(rendre_monnaie(100,0,14,5,5,1))
--- scrap.py
+++ scrap.py
@@ -1,18 +1,13 @@
-def premier(n):
-    for i in range(2,n):
-        if n % i == 0:
-            return False
-        return True
+import math
+def rac_eq_2nd_deg(a,b,c):
+    if b**2-(4*a*c)>0:
+        racines = math.sqrt(b**2-(4*a*c))
+        rac1 = (-b+racines)/(2*a)
+        rac2 = (-b-racines)/(2*a)
+        return min(rac1,rac2),max(rac1,rac2)
+    elif b**2-(4*a*c)==0:
+        return (-b/(2*a),)
+    else:
+        return ()
 
-# x = int(input())
-# if x == 1:
-#     print(1)
-# elif x == 2:
-#     print(2)
-# else:
-#      for l in range(x):
-#         if premier(l):
-#             print(l)
-
-            
-        
+print(rac_eq_2nd_deg(5,-3,-1.5))
