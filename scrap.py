08e5597ce8bbbfbf468710675a75d2e76c14c840
mahrinizar <mahrinizar01@gmail.com>
2020-10-01 12:50:43 +0200

    m

--- scrap.py
+++ scrap.py
@@ -1,13 +1,9 @@
-import math
-def rac_eq_2nd_deg(a,b,c):
-    if b**2-(4*a*c)>0:
-        racines = math.sqrt(b**2-(4*a*c))
-        rac1 = (-b+racines)/(2*a)
-        rac2 = (-b-racines)/(2*a)
-        return min(rac1,rac2),max(rac1,rac2)
-    elif b**2-(4*a*c)==0:
-        return (-b/(2*a),)
-    else:
-        return ()
+def bat(j1,j2):
 
-print(rac_eq_2nd_deg(5,-3,-1.5))
+    if (j1 ==0 and j2==2) or (j1==1 and j2==0) or (j1==2 and j2==1):
+        res = True
+    else:
+        res=False
+    return res
+
+print(bat(2,1))
