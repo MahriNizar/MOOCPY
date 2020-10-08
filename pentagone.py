57f3be871bb0df18949f79e0ab718d5ac2441e66
MahriNizar <mahrinizar01@gmail.com>
2020-10-08 22:36:25 +0200

    test

--- pentagone.py
+++ pentagone.py
@@ -1,2 +1,14 @@
-for i in range(5):
-    print(i)
+def dupliques(li):
+    res = ""
+    for i in li:
+        if li.count(i) != 1:
+            res = True
+            break
+        else:
+            res = False
+    if li == "":
+        res = False
+    return res
+
+
+print(dupliques(""))
